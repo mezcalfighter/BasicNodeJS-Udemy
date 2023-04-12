3eea3ddf5a5bb51582c83a572dcfd32a759bd5df
Emanuel Camarena <emanuel.ortega@hotmail.com>
2023-04-12 16:44:08 -0600

    Added data types

---
+++ Javascript-Review/index.js
@@ -0,0 +1,34 @@
+// Hello World - Javascript
+console.log("Hello world!");
+
+// Tipos de datos
+// Number
+let number1 = 10;
+let number2 = 20;
+console.log(number1 + number2);
+
+// String
+let myName = "Emanuel";
+console.log(`Hello, my name is ${myName}`);
+
+// Boolean
+let myBoolan = true;
+console.log(myBoolan);
+
+// Undefined - When it doesn't have a value type
+console.log(undefined);
+
+// Null - When it doesn't have a value
+console.log(null);
+
+/**
+ * Tipos de datos estructurales
+ */
+
+// Objecto
+let myObject = { name: "Emanuel", age: 26 };
+console.log(myObject);
+
+// Array
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
