6adcd84e948af35c296ab036e5bad13532698919
Emanuel Ortega <emanuel.ortega@hotmail.com>
2023-04-13 15:21:20 -0600

    Added variables

--- Javascript-Review/index.js
+++ Javascript-Review/index.js
@@ -32,3 +32,39 @@ console.log(myObject);
 // Array
 let myArray = [1, 2, 3, 4, 5];
 console.log(myArray);
+
+/**
+ * Variables - Pequeños  espacios de memoria que almacenan valores
+ * 
+ * 3 Formas de definir variables: var - let - const
+ * 
+ */
+
+// Variables con var - Var ya no se utiliza porque genera problemas en el scope ya que las crea globales
+// No se debe de utilizar let y var, es mejor solo utilizar let
+var miNombre = 'Emanuel Camarena';
+console.log(miNombre);
+
+var edad = 26;
+console.log(edad);
+
+var persona = {
+    nombre:'Emanuel',
+    apellido:'Camarena',
+    edad:26,
+    direccion:{
+        calle:'Av. Siempre Viva',
+        numeroDeCasa:123
+    },
+    ciudadesVisitadas:[
+        'Cancun',
+        'Leon',
+        'CDMX',
+        'Guadalajara'
+    ]
+}
+console.log(persona.ciudadesVisitadas);
+
+//Variables con let - Funcionan igual pero el scope es diferente
+let nombre = 'Emanuel'
+
